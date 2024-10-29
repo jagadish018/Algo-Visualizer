68b5740f0d4cebcc9f0b4d8f3a91c54e1232310d
jagadishck.018 <jagadishck.018.com>
2024-10-29 13:15:28 +0530

    intial commir

---
+++ scheduler/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import header from './components/header'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <header/>
+    </h1>
+  )
+}
